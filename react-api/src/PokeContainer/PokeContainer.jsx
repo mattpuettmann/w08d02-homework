5e7ab00e3b9889f812aa5b0fdde8a04db5dbe5fc
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-06-05 11:55:30 -0600

    working properly now

---
+++ react-api/src/PokeContainer/PokeContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PokeContainer extends Component {
+    constructor(){
+        super();
+        this.state = {
+            name: ""
+        }
+    }
+    render(){
+        return <div>
+            Poke info goes here.
+        </div>
+    }
+}
+
+
+export default PokeContainer
--- react-api/src/PokeIndex/PokeIndex.jsx
+++ react-api/src/PokeIndex/PokeIndex.jsx
@@ -2,41 +2,51 @@ import React, { Component } from 'react';
 import NameContainer from '../NameContainer/NameContainer';
 import TypeContainer from '../TypeContainer/TypeContainer';
 import AbilityContainer from '../AbilityContainer/AbilityContainer';
+import PokeContainer from '../PokeContainer/PokeContainer';
 
 class PokeIndex extends Component {
     constructor(){
         super();
         this.state = {
-            loggedIn: true
+            loggedIn: true,
+            name: null,
+            url: null,
+            type: null
         }
     }
-    changePage = (e) => {
-        console.log(e.target.id);
+
+    componentDidMount(){
+        this.showPokemon();
+    }
+    showPokemon = async () => {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+        const parsedResponse = await response.json();
+        console.log(parsedResponse);
+        console.log(parsedResponse.results)
+        console.log(parsedResponse.results[0].name)
         this.setState({
-          chosenPage: e.target.id
+            name: parsedResponse.results[0].name,
+            url: parsedResponse.results[0].url
+        })
+        const typeResponse = await fetch('https://pokeapi.co/api/v2/type')
+        const parsedTypeResponse = await typeResponse.json();
+        console.log(parsedTypeResponse.results[11].name)
+        this.setState({
+            type: parsedTypeResponse.results[11].name
         })
     }
+
     render(){
+        console.log(this.state)
         return <div>
             <h3>Welcome to your PokeDex, {this.props.username}!</h3>
-            <button onClick={this.changePage} id="name">Search by Name</button>
-            <button onClick={this.changePage} id="type">Search by Type</button>
-            <button onClick={this.changePage} id="ability">Search by Ability</button>
-            {
-                this.state.chosenPage === null ?
-                    null
-                    :
-                    this.state.chosenPage === "name" ?
-                        <NameContainer></NameContainer>
-                        :
-                        this.state.chosenPage === "type" ?
-                            <TypeContainer></TypeContainer>
-                            :
-                            this.state.chosenPage === "ability" ?
-                                <AbilityContainer></AbilityContainer>
-                                :
-                                null
-            }
+            <h4>Crikey! Take a look at this one!</h4>
+            Name: {this.state.name}<br/>
+            URL: {this.state.url}<br/>
+            Type: {this.state.type}
+
+
+
         </div>
     }
 }
